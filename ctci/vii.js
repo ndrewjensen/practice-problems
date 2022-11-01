9f07842442387e5f192e9197c78b048347c9e3ac
Andrew Jensen <andrewkjensen@gmail.com>
2022-11-01 16:02:52 -0700

    solved in O(n*m)

--- ctci/vii.js
+++ ctci/vii.js
@@ -5,5 +5,76 @@
 // Print teh location of each permutation.
 
 // ex:
-// s = abbc
-// b = cbabadcbbabbcbabaabccbabc
+// s = "abbc"
+// b = "cbabadcbbabbcbabaabccbabc"
+
+/** input: string or array
+ * output: frequency counter like {a:1,b:3,...}
+ */
+
+function makeFrequencyCounter(input) {
+  const output = {};
+
+  for (let char of input) {
+    let current = output[char] || 0;
+    current++;
+    output[char] = current;
+  }
+
+  return output;
+}
+
+/** input two freq. counters with equal count of characters 
+ *    as objects like {a:1,b:3,...}
+ *  output boolean, true if all frequencies are equal.
+ */
+
+function compareFrequencyCounters(counter1, counter2) {
+  for (let char in counter1) {
+    if (counter1[char] !== counter2[char]) {
+      return false
+    }
+  }
+  return true
+}
+
+/** input: shortString, longString
+ *
+ * print: all permutations of shortString found in LongString
+ *
+ * output: none
+ */
+
+function printPermutations(shortString, longString) {
+  if (longString.length < shortString.length) return
+
+  const freq = makeFrequencyCounter(shortString);
+  let len = shortString.length;
+  let slidingWindow = longString.slice(0, len);
+  const windowFreq = makeFrequencyCounter(slidingWindow);
+
+  let isPermutation = compareFrequencyCounters(freq, windowFreq);
+  isPermutation && console.log(slidingWindow);
+
+  let i = len;
+  while (i < longString.length) {
+    let rem = longString[i - len];
+    let add = longString[i];
+    windowFreq[rem]--;
+    windowFreq[add] = windowFreq[add]
+      ? windowFreq[add] + 1
+      : 1
+    slidingWindow = longString.slice(i - len + 1, i+1)
+    if (isPermutation) {
+      if (rem === add) {
+        console.log(slidingWindow);
+      } else {
+        isPermutation = false;
+      }
+    } else {
+      isPermutation = compareFrequencyCounters(freq, windowFreq);
+      isPermutation && console.log(slidingWindow);
+    }
+    i++;
+  }
+}

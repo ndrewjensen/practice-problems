cac902113ffa299c4caa2153dc879293227c2b88
Andrew Jensen <andrewkjensen@gmail.com>
2022-12-01 14:03:34 -0800

    complete 4.3

--- 04.ts
+++ 04.ts
@@ -1,81 +1,79 @@
 namespace fourpointone {
-
-interface GNode {
-  val: string | number;
-  adj: GNode[];
-  qnext: GNode | null;
-}
-
-class GNode {
-  constructor(val: string, adj: GNode[] = []) {
-    this.val = val;
-    this.adj = adj;
-    this.qnext = null;
-  }
-}
-
-interface GQueue {
-  head: GNode | null;
-  tail: GNode | null;
-  length: number;
-}
-
-class GQueue {
-  constructor () {
-    this.head = null;
-    this.tail = null;
-    this.length = 0;
+  interface GNode {
+    val: string | number;
+    adj: GNode[];
+    qnext: GNode | null;
   }
 
-  isEmpty(): boolean {
-    return this.length === 0;
-  }
-
-  add(node: GNode): GNode {
-    if (!this.head) {
-      this.head = node;
-    } else {
-      this.tail!.qnext = node;
+  class GNode {
+    constructor(val: string, adj: GNode[] = []) {
+      this.val = val;
+      this.adj = adj;
+      this.qnext = null;
     }
-    this.tail = node;
-    this.length ++;
-    return node;
-
   }
 
-  remove(): GNode | null {
-    if (this.isEmpty()) return null;
-    let node: GNode = this.head!;
-    this.head = this.head!.qnext;
-    node.qnext = null;
-    this.length --;
-    return node;
+  interface GQueue {
+    head: GNode | null;
+    tail: GNode | null;
+    length: number;
   }
 
-  peek(): GNode | null {
-    return this.head;
+  class GQueue {
+    constructor() {
+      this.head = null;
+      this.tail = null;
+      this.length = 0;
+    }
+
+    isEmpty(): boolean {
+      return this.length === 0;
+    }
+
+    add(node: GNode): GNode {
+      if (!this.head) {
+        this.head = node;
+      } else {
+        this.tail!.qnext = node;
+      }
+      this.tail = node;
+      this.length++;
+      return node;
+    }
+
+    remove(): GNode | null {
+      if (this.isEmpty()) return null;
+      let node: GNode = this.head!;
+      this.head = this.head!.qnext;
+      node.qnext = null;
+      this.length--;
+      return node;
+    }
+
+    peek(): GNode | null {
+      return this.head;
+    }
   }
-}
 
-interface Graph {
-  nodes: GNode[];
-}
-
-class Graph {
-  constructor(nodes: GNode[]) {
-    this.nodes = nodes;
+  interface Graph {
+    nodes: GNode[];
   }
 
-  add(node: GNode): void {
-    this.nodes.push(node);
-  }
+  class Graph {
+    constructor(nodes: GNode[]) {
+      this.nodes = nodes;
+    }
 
-  remove(node: GNode): void {
-    const index = this.nodes.indexOf(node);
-    if (index !== -1) this.nodes.splice(index,1)
-  }
+    add(node: GNode): void {
+      this.nodes.push(node);
+    }
 
-  /** 4.1
+    remove(node: GNode): void {
+      const index = this.nodes.indexOf(node);
+      if (index !== -1) this.nodes.splice(index, 1);
+    }
+
+    /** 4.1
   Route Between Nodes: Given a directed graph and two nodes (S and E),
   design an algorithm to find out whether there is a route from S to E.
 
@@ -92,63 +90,59 @@ class Graph {
 
   */
 
-  checkRouteExists (S: GNode, E: GNode): boolean {
-    const queue = new GQueue();
-    let checked  = new Set;
-    queue.add(S);
-    let routeExists = false;
-    while (!queue.isEmpty()) {
-      let current = queue.remove();
-      checked.add(current);
-      if (current === E) routeExists = true;
-      for (let sibling of current!.adj) {
-        if (!checked.has(sibling)) queue.add(sibling);
+    checkRouteExists(S: GNode, E: GNode): boolean {
+      const queue = new GQueue();
+      let checked = new Set();
+      queue.add(S);
+      let routeExists = false;
+      while (!queue.isEmpty()) {
+        let current = queue.remove();
+        checked.add(current);
+        if (current === E) routeExists = true;
+        for (let sibling of current!.adj) {
+          if (!checked.has(sibling)) queue.add(sibling);
+        }
       }
-
+      return routeExists;
     }
-    return routeExists;
   }
 
+  /** 4.1 Test */
+  //trivial
+  let S = new GNode("S1");
+  let E = new GNode("E1");
+  let G1 = new Graph([S, E]);
+  //G1.checkRouteExists(S,S) => true
+  //G1.checkRouteExists(S,E) => false
+
+  //simlple, adjacent nodes
+  let S2 = new GNode("S1");
+  let E2 = new GNode("E1");
+  let G2 = new Graph([S2, E2]);
+  S2.adj.push(E2);
+  //G2.checkRouteExists(S2,E2) => true
+  //G2.checkRouteExists(E2,S2) => false
+
+  //multiple degrees of relationship
+  let S3 = new GNode("S1");
+  let A3 = new GNode("A1");
+  let B3 = new GNode("B1");
+  let C3 = new GNode("C1");
+  let D3 = new GNode("D1");
+  let E3 = new GNode("E1");
+  let F3 = new GNode("F1");
+
+  let G3 = new Graph([S3, A3, B3, C3, D3, F3, E3]);
+  S3.adj.push(A3, B3);
+  A3.adj.push(C3, D3);
+  C3.adj.push(B3, A3);
+  D3.adj.push(S3, F3);
+  F3.adj.push(E3);
+
+  //G3.checkRouteExists(S3,E3) => true
+  //G3.checkRouteExists(A3,E3) => true
+  //G3.checkRouteExists(B3,E3) => false
 }
-
-/** 4.1 Test */
-//trivial
-let S = new GNode("S1");
-let E = new GNode("E1");
-let G1 = new Graph([S,E]);
-//G1.checkRouteExists(S,S) => true
-//G1.checkRouteExists(S,E) => false
-
-//simlple, adjacent nodes
-let S2 = new GNode("S1");
-let E2 = new GNode("E1");
-let G2 = new Graph([S2,E2]);
-S2.adj.push(E2);
-//G2.checkRouteExists(S2,E2) => true
-//G2.checkRouteExists(E2,S2) => false
-
-//multiple degrees of relationship
-let S3 = new GNode("S1");
-let A3 = new GNode("A1");
-let B3 = new GNode("B1");
-let C3 = new GNode("C1");
-let D3 = new GNode("D1");
-let E3 = new GNode("E1");
-let F3 = new GNode("F1");
-
-let G3 = new Graph([S3,A3,B3,C3,D3,F3,E3]);
-S3.adj.push(A3,B3);
-A3.adj.push(C3,D3);
-C3.adj.push(B3,A3);
-D3.adj.push(S3,F3)
-F3.adj.push(E3)
-
-//G3.checkRouteExists(S3,E3) => true
-//G3.checkRouteExists(A3,E3) => true
-//G3.checkRouteExists(B3,E3) => false
-
-}
-
 
 /** 4.2
  Minimal Tree: Given a sorted (increasing order) array with unique integer
@@ -169,7 +163,6 @@ example: [1,2,3,4,5,6,7,8,9] =>
   1                9
 
 */
-
 
 interface BSTNode {
   val: number;
@@ -197,15 +190,15 @@ class BST {
   add(addNode: BSTNode, currNode: BSTNode | null = this.root): void {
     if (!currNode) {
       this.root = addNode;
-    } else if (addNode.val < currNode.val){
+    } else if (addNode.val < currNode.val) {
       if (currNode.left) {
-        this.add(addNode,currNode.left)
+        this.add(addNode, currNode.left);
       } else {
         currNode.left = addNode;
       }
     } else {
       if (currNode.right) {
-        this.add(addNode,currNode.right)
+        this.add(addNode, currNode.right);
       } else {
         currNode.right = addNode;
       }
@@ -213,25 +206,29 @@ class BST {
   }
 }
 
-function minimalBST(ints: number[], tree: BST): BST {
+function minimalBST(
+  ints: number[],
+  tree: BST = new BST(),
+  currNode: BSTNode
+): BST {
   if (ints.length === 1) {
     tree.add(new BSTNode(ints[0]));
-    return tree
+    return tree;
   } else if (ints.length === 0) {
-    return tree
+    return tree;
   } else {
-    let length  = ints.length
-    tree.add(new BSTNode(ints[Math.floor(length/2)]))
-    minimalBST(ints.slice(0,Math.floor(length/2)),tree)
+    let length = ints.length;
+    let node = new BSTNode(ints[Math.floor(length / 2)]);
+    tree.add(node, currNode);
+    minimalBST(ints.slice(0, Math.floor(length / 2)), tree, node);
     if (length % 2 === 1) {
-      minimalBST(ints.slice(Math.ceil(length/2),length),tree)
+      minimalBST(ints.slice(Math.ceil(length / 2), length), tree, node);
     } else {
-      minimalBST(ints.slice(Math.ceil(length/2)+1,length),tree)
+      minimalBST(ints.slice(Math.ceil(length / 2) + 1, length), tree, node);
     }
   }
-  
-  return tree
 
+  return tree;
 }
 
 /* 
@@ -288,3 +285,74 @@ class BSTQueue {
   }
 }
  */
+
+/** 4.3
+List of Depths: Given a binary tree, design an algorithm which creates a linked
+list of all the nodes at each depth (e.g., if you have a tree with depth D,
+you'll have D linked lists).
+
+this needs to be breadth first. by using a queue, the 
+
+declare an array to store the queue
+push null, then push self.
+i = 1
+while i <= queue.length
+  if i is null, i++
+  else if i-1 is null, push null, then push non-null children
+  else push non-null children
+  i++
+
+declare an array to store the lists (in the args?)
+
+loop through queue while i< queue.length
+  if i is null
+    let list be new ll
+    push into array
+    i++ 
+  while i isn't null
+    add each value to the list
+    i++
+
+return the array
+ */
+
+interface LLNode {
+  val: BSTNode;
+  next: LLNode | null;
+}
+
+class LLNode {
+  constructor(val: BSTNode) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+function listDepths(bt: BST): LLNode[] {
+  if (!bt.root) return [];
+  let queue: (null | LLNode)[] = [null, new LLNode(bt.root!)];
+  let lists: LLNode[] = [];
+  let i: number = 1;
+
+  while (i <= queue.length) {
+    if (!queue[i]) {
+      i++;
+      continue;
+    }
+    if (!queue[i - 1]) queue.push(null);
+    if (queue[i]!.val.left) queue.push(new LLNode(queue[i]!.val.left!));
+    if (queue[i]!.val.right) queue.push(new LLNode(queue[i]!.val.right!));
+    i++;
+  }
+
+  i = 1;
+  while (i < queue.length) {
+    if (!queue[i - 1]) {
+      lists.push(queue[i]!);
+    } else {
+      queue[i - 1]!.next = queue[i];
+    }
+    i++;
+  }
+  return lists;
+}

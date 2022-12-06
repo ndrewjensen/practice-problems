9bf0fd99dd2b78dd37c0815c3bdde7b06690dcae
Andrew Jensen <andrewkjensen@gmail.com>
2022-12-06 14:48:02 -0800

    complete dec 1

--- advent-of-code/src/App.tsx
+++ advent-of-code/src/App.tsx
@@ -1,12 +1,20 @@
 import React from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import First from "./first/first";
+import { Card } from "react-bootstrap";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
+    <div className='App'>
+      <h1 className='m-3'>Advent of Code</h1>
+      <div className='container'>
+        <div className='row'>
+          <Card>
+            <First />
+          </Card>
+        </div>
+      </div>
     </div>
   );
 }
-
-export default App;
---
+++ advent-of-code/src/first/first.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { input } from "./first-input";
+
+function updateTopThree(nums:number[]=[0,0,0],addNum:number) {
+  if (addNum > nums[0]) {
+    nums.unshift(addNum);
+    nums.pop();
+  } else if (addNum > nums[1]) {
+    [nums[1],nums[2]] = [addNum,nums[1]];
+  } else {
+    nums[2] = addNum;
+  }
+  return nums;
+}
+export default function First() {
+const topThreeCals:number[] = [0,0,0];
+
+  let currentCal = 0;
+  for (const cal of input) {
+    if (!cal) {
+      if (currentCal > topThreeCals[2]) {
+        updateTopThree(topThreeCals,currentCal)
+      }
+      currentCal = 0;
+    } else {
+      currentCal += Number(cal);
+    }
+  }
+
+  return (
+    <div className='First card-body'>
+      <h5 className='card-title'>December 1st: Calorie Counting</h5>
+      <p className='card-text'>
+        The max calories is {topThreeCals[0]}.
+      </p>
+      <p className='card-text'>
+        The top three are: {topThreeCals[0]}, {topThreeCals[1]}, and {topThreeCals[2]}. 
+      </p>
+      <p className='card-text'>
+        The sum of the top three is: {topThreeCals[0] + topThreeCals[1] + topThreeCals[2]}. 
+      </p>
+    </div>
+  );
+}

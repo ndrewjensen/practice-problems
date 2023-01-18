53553847c27c407ca45d781ef09ea4f8e6d79a52
Andrew Jensen <andrewkjensen@gmail.com>
2023-01-18 13:19:03 -0800

    river lengths with Brenda

--- whiteboarding/brenda.ts
+++ whiteboarding/brenda.ts
@@ -45,3 +45,151 @@ function biggestBeachStretch(
 
 	return maxSize ? [maxSize, ourSpot] : "no space";
 }
+
+/*
+You're given a two-dimensional array (a matrix) of potentially unequal height 
+and width containing only 0's and 1's.
+
+Each 0 represents land, and each 1 represents part of a river.
+A river consists of any number of 1's that are either horizontally or vertically
+adjacent (but not diagonally adjacent).
+The number of adjacent 1's forming a river determine its size.
+
+Note that a river can twist. In other words, it doesn't have to be a straight
+vertical line or a straight horizontal line;
+it can be L-shaped, for example.
+
+Write a function that returns an array of the sizes of all rivers represented in
+the input matrix.
+
+The sizes don't need to be in any particular order.
+
+Sample:
+matrix = [
+[1,0,0,1,0],
+[1,0,1,0,0],
+[0,0,1,0,1],
+[1,0,1,0,1],
+[1,0,1,1,0],
+]
+
+mid-river
+matrix = [
+[1,0,0,0,0],
+[1,0,1,1,0],
+[0,0,1,0,1],
+[1,0,1,0,1],
+[1,0,1,1,0],
+]
+
+matrix = [
+[1,0,0,0,0],
+[1,0,1,1,0],
+[0,1,1,0,1],
+[1,0,1,0,1],
+[1,0,1,1,0],
+]
+[0,0,0,0,0],
+[0,0,1,1,0],
+[1,0,1,0,1],
+[1,1,1,0,1],
+[1,0,1,1,0],
+]
+[0,0,0,0,0],
+[0,0,1,1,0],
+[1,0,1,0,1],
+[1,1,1,0,1],
+[1,0,1,1,0],
+]
+
+Sample Output
+[1, 2, 2, 2, 5]
+
+declare an empty output array
+iterate through each row, then column
+	declare river size and set to 0
+	when reaching a 1: 
+		flip it to 0
+		check right and down for another 1 (while loop)
+			increment river size
+		add river size to output array	
+
+
+
+recursive helper - inputs: matrix, i, j
+	check right
+	check down		
+	return riverlength? or return undefined and increment river size from function
+	above
+*/
+
+function findRiverLengths(m: number[][]): number[] {
+	if (m.length === 0) return [];
+	const sizes: number[] = [];
+	const height = m.length;
+	const width = m[0].length;
+
+	function followRiver(i: number, j: number, size: number[] = [0]): number {
+		size[0]++;
+		m[i][j] = 0;
+		if (i+1 < height && m[i + 1][j] === 1) followRiver(i + 1, j, size);
+		if (i - 1 >= 0 && m[i - 1][j] === 1) followRiver(i - 1, j, size);
+		if (j + 1 < width && m[i][j + 1] === 1) followRiver(i, j + 1, size);
+		if (j - 1 >= 0 && m[i][j - 1] === 1) followRiver(i, j - 1, size);
+		return size[0];
+	}
+
+	for (let i = 0; i < height; i++) {
+		for (let j = 0; j < width; j++) {
+			if (m[i][j] === 1) {
+				sizes.push(followRiver(i, j, [0]));
+			}
+		}
+	}
+	return sizes;
+}
+
+
+/* function findRiverLengths2 (land) {
+	const riverLengths = [];
+	const visited = {};
+  
+	for (let i = 0; i < land.length; i++) {
+	  for (let j = 0; j < land[i].length; j++) {
+		let coordinate = land[i][j];
+  
+		if (coordinate === 1 && !visited[`${i}-${j}`]) {
+		  let riverLength = dfs([i,j], land, visited);
+		  riverLengths.push(riverLength);
+		}
+	  }
+	}
+  
+	return riverLengths;
+  }
+  
+  
+  function dfs (start, land, visited) {
+	const directions = [[1,0], [-1,0], [0,1], [0,-1]];
+	const stack = [start];
+	let riverLength = 0;
+  
+	while (stack.length) {
+	  const [x,y] = stack.pop();
+	  if (visited[`${x}-${y}`] === undefined) {
+		visited[`${x}-${y}`] = true;
+		if (land[x][y] === 1) {
+		  riverLength++;
+		  for (let [x1,y1] of directions) {
+			let newX = x + x1;
+			let newY = y + y1;
+			if (newX >=0 && newX < land.length && newY >=0 && newY <= land[0].length) {
+			  stack.push([newX, newY]);
+			}
+			}
+		  }
+	  }
+	}
+  
+	return riverLength;
+  } */
